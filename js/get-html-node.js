afd80b4026fc34f24eb43eb3d062e610dddcfc41
Andrey Titov <31188661+jsandre@users.noreply.github.com>
2019-08-22 16:54:00 +0300

    Add files via upload

---
+++ js/get-html-node.js
@@ -0,0 +1,9 @@
+// $(function() {
+//   var $listItemHTML = $('li').html();
+//   $('li').append('<i>' + $listItemHTML + '</i>');
+// });
+
+$(function(){
+  var $ListItemHTML = $('li').html();
+  $('li').append('<i>' + $ListItemHTML + '</i>');
+});
